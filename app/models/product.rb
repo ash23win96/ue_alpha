89c550b7cb611daa8ffd6f81d5eeb5cae6006a22
Ashwin <win.ash23@gmail.com>
2023-11-24 13:07:24 +0530

    Added validations to Product model and updated views as required

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 10, maximum: 200 }
+  validates :price, presence: true, numericality: true
+  validates :category_id, presence: true
 end
